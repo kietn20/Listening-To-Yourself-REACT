63fa9b288857ea5f92ce0631071fd2a55330d653
kietn20 <kietnguyen3698@gmail.com>
2023-08-02 14:41:11 -0700

    fixing broken bgs x4

--- client/src/components/Moods.jsx
+++ client/src/components/Moods.jsx
@@ -4,10 +4,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
-const bg = {
-	"background-image": url("./card2bg.svg"),
-};
-
 export const Moods = ({ access_token, setToken, getRefreshToken }) => {
 	const [songObjects, setSongObjects] = useState([]);
 	const [ids, setIds] = useState([]);
@@ -156,7 +152,7 @@ export const Moods = ({ access_token, setToken, getRefreshToken }) => {
 						</div>
 					</div>
 					<div className="analysis-div">
-						<div style={bg} className="card3 analysis-number">
+						<div className="card3 analysis-number">
 							<h1>{audio[2]}</h1>
 						</div>
 						<div className="analysis-text">
